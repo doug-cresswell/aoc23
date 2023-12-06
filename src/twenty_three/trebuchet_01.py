ab2170c27d414107784b562c63ebe38c2a350229
doug-cresswell <104023005+doug-cresswell@users.noreply.github.com>
2023-12-06 10:26:10 +0000

    trebuchet part two

---
+++ src/twenty_three/trebuchet_01.py
@@ -0,0 +1,116 @@
+from pprint import pprint
+
+
+def parse(line: str) -> int:
+    """Concatenate the first and last number from a string to form a two digit number."""
+
+    digits = [str(n) for n in range(0, 10)]
+    nums = [s for s in line if s in digits]
+    first, last = nums[0], nums[-1]
+    return int(f"{first}{last}")
+
+
+def solution_one(txt: str) -> int:
+    """Sum the array of two digit numbers parsed from the text."""
+
+    nums = [parse(line) for line in txt.split("\n")]
+    return sum(nums)
+
+
+def parse_two(line: str) -> int:
+    """
+    Concatenate the first and last number from a string to form a two digit number.
+
+    one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+    """
+
+    def first_num(line: str, reverse: bool = False) -> int:
+        # Find the first single digit number (numeral or written) and convert to an int
+        # When reverse is False, chunk from left -> right
+        # Otherwise, chunk from right -> left
+        # Note: The order of characters in string is still read left -> right
+        # Example
+        # >>> first_num("7pqrstsixteen", reverse=True)
+        # >>> 6
+
+        numerals = range(1, 10)
+        names = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+        ]
+        digit_map = dict(
+            zip(
+                names,
+                numerals,
+            )
+        )
+
+        start = min(len(n) for n in names)
+        stop = len(line)
+
+        # Would it be better to use queues?
+        rng = range(stop)
+        if reverse:
+            rng = rng[::-1]
+        for i in rng:
+            try:
+                return int(line[i])
+            except ValueError:
+                if reverse:
+                    substring = line[i:]
+                else:
+                    substring = line[: i + 1]
+                if len(substring) >= start:
+                    # TODO: Only check for names <= len(substring)
+                    for name, numeral in digit_map.items():
+                        if name in substring:
+                            return numeral
+
+    first = first_num(line)
+    last = first_num(line, reverse=True)
+    return int(f"{first}{last}")
+
+
+def solution_two(txt: str) -> int:
+    nums = [parse_two(line) for line in txt.split("\n")]
+    return sum(nums)
+
+
+def main():
+    def read(path):
+        with open(path) as f:
+            txt = f.read().strip()
+        return txt
+
+    stem = "2023_01"
+
+    input_text = dict(
+        example=read(f"inputs/{stem}_example.txt"),
+        actual=read(f"inputs/{stem}.txt"),
+    )
+
+    answers = {
+        "part_one": {k: solution_one(v) for k, v in input_text.items()},
+        "part_two": {k: solution_two(v) for k, v in input_text.items()},
+    }
+
+    pprint(answers)
+
+
+if __name__ == "__main__":
+    # main()
+    # lines = ["eighttwothree-83", "xtwone3four-24", "zoneight234-14"]
+    # for line in lines:
+    #     a = parse_two(line)
+    #     print(line, a)
+    #     print()
+
+    a = parse("one")
+    print(a)
--- test/2023/test_trebuchet_01.py
+++ test/2023/test_trebuchet_01.py
@@ -2,11 +2,11 @@ from textwrap import dedent
 
 import pytest
 
-from src.app.trebuchet_01 import parse, solution
+from src.twenty_three.trebuchet_01 import parse, parse_two, solution_one, solution_two
 
 
 @pytest.fixture
-def input():
+def txt():
     s = """1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
@@ -19,8 +19,25 @@ def answer():
     return 142
 
 
+@pytest.fixture
+def txt_two():
+    s = """two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen"""
+    return dedent(s)
+
+
+@pytest.fixture
+def answer_two():
+    return 281
+
+
 @pytest.mark.parametrize(
-    "input,expected",
+    "txt,expected",
     [
         ("1abc2", 12),
         ("pqr3stu8vwx", 38),
@@ -28,15 +45,37 @@ def answer():
         ("treb7uchet", 77),
     ],
 )
-def test_parse(input, expected):
-    print(input, expected)
-    actual = parse(input)
+def test_parse(txt, expected):
+    print(txt, expected)
+    actual = parse(txt)
     assert actual == expected
 
 
-def test_solution(input, answer):
-    actual = solution(input)
+@pytest.mark.parametrize(
+    "txt,expected",
+    [
+        ("1abc2", 12),
+        ("pqr3stu8vwx", 38),
+        ("a1b2c3d4e5f", 15),
+        ("treb7uchet", 77),
+        ("two1nine", 29),
+        ("eightwothree", 83),
+        ("abcone2threexyz", 13),
+        ("xtwone3four", 24),
+        ("4nineeightseven2", 42),
+        ("zoneight234", 14),
+        ("7pqrstsixteen", 76),
+    ],
+)
+def test_parse_two(txt, expected):
+    print(txt, expected)
+    actual = parse_two(txt)
+    assert actual == expected
+
+
+def test_solution(txt, txt_two, answer, answer_two):
+    actual = solution_one(txt)
     assert actual == answer
-    assert actual == answer
-    assert actual == answer
-    assert actual == answer
+
+    actual = solution_two(txt_two)
+    assert actual == answer_two
